bfb883da084f26cb6ebae9e32c693b945646b92c
Tapan Pandita <tapan.pandita@gmail.com>
2019-08-07 00:51:38 +0530

    Added another WIP crawler that uses producer-consumer pattern

--- aiocrawler.py
+++ aiocrawler.py
@@ -101,3 +101,116 @@ class AIOCrawler:
         await self.session.close()
 
         return sitemap
+
+
+
+class AnotherCrawler:
+
+    def __init__(self, init_url: str, depth: int = 1, concurrency: int = 1000,
+                 user_agent: str = 'AnotherCrawler') -> None:
+        '''
+        Initialize State
+        '''
+        self.init_url = init_url
+        self.depth = depth
+        self.concurrency = concurrency
+        self.user_agent = user_agent
+
+        self.base_url: str = '{}://{}'.format(
+            urlparse(self.init_url).scheme,
+            urlparse(self.init_url).netloc,
+        )
+        self.crawled_urls: Set[str] = set()
+        self.session: ClientSession = ClientSession()
+        self.task_queue: asyncio.Queue = asyncio.Queue(maxsize=concurrency)
+        self.results_queue: asyncio.Queue = asyncio.Queue(maxsize=concurrency)
+
+    async def _make_request(self, url: str) -> bytes:
+        '''
+        Wrapper on aiohttp to make get requests on a shared session
+        '''
+        logging.debug('Fetching: {url}')
+        headers = {
+            'User-Agent': self.user_agent,
+        }
+        async with self.session.get(url, headers=headers) as response:
+            html = await response.read()
+            return html
+
+    def find_links(self, html: bytes) -> Set[str]:
+        '''
+        Find all the links in passed html
+        '''
+        soup = BeautifulSoup(html, 'html.parser')
+        links = {
+            urljoin(self.base_url, a['href'])
+            for a in soup.select('a[href]')
+            if urljoin(self.base_url, a['href']).startswith(self.base_url)
+        }
+        return links
+
+    async def crawl_page(self, url: str):
+        '''
+        Request a webpage and return all relevant data from it
+        '''
+        html = await self._make_request(url)
+        links = self.find_links(html)
+        return url, links, html
+
+    async def consumer(self):
+        '''
+        '''
+        while True:
+            url, depth= await self.task_queue.get()
+            print(f'Working on {url} at {depth}')
+
+            if depth >= self.depth:
+                self.task_queue.task_done()
+                print('Max depth reached')
+                break
+
+            if url in self.crawled_urls:
+                continue
+
+            self.crawled_urls.add(url)
+            data = await self.crawl_page(url)
+            await self.results_queue.put((data, depth))
+            self.task_queue.task_done()
+
+    async def producer(self):
+        '''
+        '''
+        while True:
+            data, depth = await self.results_queue.get()
+            url, links, html = data
+
+            curr_depth = depth + 1
+
+            for url in links:
+                await self.task_queue.put((url, curr_depth))
+
+            self.results_queue.task_done()
+
+    async def crawl(self):
+        '''
+        '''
+        self.task_queue.put_nowait((self.init_url, 0))
+        producers = [asyncio.create_task(self.producer()) for i in range(2)]
+        consumers = [asyncio.create_task(self.consumer()) for i in range(self.concurrency)]
+
+        await asyncio.gather(*producers, *consumers)
+
+        # results = [await task for task in asyncio.as_completed(consumers)]
+
+        print('Waiting for tasks_queue to join')
+        await self.task_queue.join()
+        print('Waiting for results_queue to join')
+        await self.results_queue.join()
+
+        print('Waiting for producers to cancel')
+        for producer in producers: producer.cancel()
+        print('Waiting for consumers to cancel')
+        for consumer in consumers: consumer.cancel()
+
+        print('Waiting for session to close')
+        await self.session.close()
